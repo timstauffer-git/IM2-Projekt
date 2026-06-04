e22a475cda73eedf40deba44e1a59e76e7e970eb
timstauffer-git <tim.stauffer@stud.fhgr.ch>
2026-06-04 13:17:32 +0200

    Kartenanzeigedauer 2

--- js/script.js
+++ js/script.js
@@ -331,7 +331,7 @@ function checkForMatch() {
       firstCard.classList.remove("is-flipped");
       secondCard.classList.remove("is-flipped");
       resetTurn();
-    }, 1800);
+    }, 2100);
   }
 }
 
